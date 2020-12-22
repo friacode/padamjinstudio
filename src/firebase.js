d2d0029d4417b94eb65cf5bb6d24bc6e9eba970a
friacode <friacode@gmail.com>
2020-12-22 09:57:26 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3uGMPpcxt3b_uICiWzuHshQl6ydt-GF8",
+  authDomain: "padamjinstudio-958ae.firebaseapp.com",
+  projectId: "padamjinstudio-958ae",
+  storageBucket: "padamjinstudio-958ae.appspot.com",
+  messagingSenderId: "196526446183",
+  appId: "1:196526446183:web:708a4ae8e95546464566c2",
+  measurementId: "G-CD73S2BY35"
+};
+
+export default firebase.initializeApp(firebaseConfig);
